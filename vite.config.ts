a57bc75c195913bd084532b4d17dec43b985cd76
JJthang <thangpdph@gmail.com>
2025-10-13 09:12:00 +0700

    setup base

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path';
+import svgr from 'vite-plugin-svgr';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+     svgr()
+  ],
+  server: {
+    port: 3000,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
